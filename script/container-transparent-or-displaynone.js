136aa13690a6d38160de4650719ee1b282f4be28
Nihit Bansal <Nihit.Bansal@cvent.com>
2021-02-19 21:07:48 +0530

    Made game

---
+++ script/container-transparent-or-displaynone.js
@@ -0,0 +1,6 @@
+function setContainerTransparentOrDisplaynone()
+{
+    "internet explorer" == browserName && browserVersion <= 8 ? containerDiv.setAttribute("class", "displaynone") : containerDiv.setAttribute("class", "transparent")
+}
+var containerDiv = document.getElementById("container");
+setContainerTransparentOrDisplaynone();
