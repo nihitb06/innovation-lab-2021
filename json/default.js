1b2382f37887eaa72d31b2de60cdbc3d4c87f14a
Nihit Bansal <Nihit.Bansal@cvent.com>
2021-03-13 17:27:15 +0530

    Final-Round: Minor fine-tuning.

---
+++ json/default.js
@@ -0,0 +1,48 @@
+var data = {
+    "page-title": "Cvent, Year in Review 2020",
+    "title": "2020, Year\n in Review",
+    "level-1": {
+        "title": "Pre-Pandemic Expectations",
+        "section-1": {
+            "title": "Upwards Growth",
+            "labelsX": ['2000', '2005', '2016', '2019'],
+            "data": [1, 2, 3, 4]
+        },
+        "section-2": {
+            "title": "Global Momentum"
+        },
+        "section-3": {
+           "title": "Future Looked Great",
+            "red-text": ['G', 'O'],
+            "blue-text": ['C', 'V', 'E', 'N', 'T']
+        }
+    },
+    "level-2": {
+        "title": " Covid-19 Hits! <br />",
+        "subtitle": "The entire industry shaken to the core.",
+        "data": [
+            {
+                "title": "Unprecedented Spread",
+                "labelsX": ['200K', '300K', '400K', '500K', '600K'],
+                "labelsY": ['June', 'July', 'October', 'November'],
+                "data": [1, 2, 3, 5]
+            },
+            {
+                "title": "Events Industry Impacted",
+                "labelsX": ['100K', '200K', '300K', '400K', '500K'],
+                "labelsY": ['April', 'June', 'August', 'October'],
+                "data": [4, 3, 1, 0]
+            },
+            {
+                "title": "Cvent Impacted",
+                "labelsX": ['50K', '100K', '150K', '200K', '250K'],
+                "labelsY": ['March', 'June', 'August', 'October'],
+                "data": [3, 1, 2, 4]
+            }
+        ]
+    },
+    "level-3": {
+        "title": "Swift Response",
+        "subtitle": "Early Responders to the Pandemic"
+    }
+};
--- script/preloader-transparent-or-displaynone.js
+++ script/preloader-transparent-or-displaynone.js
@@ -1,1 +1,5 @@
-function setPreloaderTransparentOrDisplaynone(){"internet explorer"==browserName&&browserVersion<=8?preloaderDiv.setAttribute("class","displaynone"):preloaderDiv.setAttribute("class","transparent")}var preloaderDiv=document.getElementById("preloader");setPreloaderTransparentOrDisplaynone();
+function setPreloaderTransparentOrDisplaynone(){
+    "internet explorer"==browserName&&browserVersion<=8?preloaderDiv.setAttribute("class","displaynone"):preloaderDiv.setAttribute("class","transparent")
+}
+var preloaderDiv=document.getElementById("preloader");
+setPreloaderTransparentOrDisplaynone();
